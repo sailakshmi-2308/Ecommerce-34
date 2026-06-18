b31a8bd9c4696f3cfb1cd83720ba51a9ee8507c0
Sai Lakshmi <sailakshmi@codegnan.com>
2026-06-18 11:00:21 +0530

    add product

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
 export default function AddProduct() {
+
+    const [formData,setFormData]=useState({
+
+        title:"",
+        Description:"",
+        About_item:"",
+        quantity:"",
+        price:"",
+        category
+    })
+
+    const [file,setFile]=useState(null)
+
+    function handleChange(){
+
+    }
   return (
     <div>
-      Add productssss
+    <form>
+
+
+        <div>
+            <label htmlFor="">Product title</label>
+            <input type="text" 
+            placeholder="enter product title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            />
+        </div>
+
+
+
+    </form>
     </div>
   )
 }
