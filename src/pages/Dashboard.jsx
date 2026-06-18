e1a783a4c85f7377cac96617b642e9e8a4dade68
Sai Lakshmi <sailakshmi@codegnan.com>
2026-06-18 10:36:02 +0530

    navbar page done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Dashboard() {
+  const navigate=useNavigate()
 
   const admin=JSON.parse(localStorage.getItem("admin"))
   return (
@@ -9,7 +11,7 @@ export default function Dashboard() {
      <h2><b>Welcome:</b>{admin?.adminemail}</h2>
      <p><b>Admin ID:</b>{admin?.adminid}</p>
       <p><b>Admin Name:</b>{admin?.adminname}</p>
-     <button>Go to Admin Products</button>
+     <button onClick={()=>navigate("/admin-products")}>Go to Admin Products</button>
     </div>
   )
 }
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,15 +1,52 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import "./Navbar.css"
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function Navbar() {
+
+  const [user, setUser] = useState(null)
+  const location = useLocation()
+  const navigate=useNavigate()
+
+  useEffect(() => {
+    const admin = JSON.parse(localStorage.getItem("admin"))
+    setUser(admin)
+  }, [location])
+
+  function logout(){
+    localStorage.removeItem("admin")
+    navigate("/login")
+  }
+
   return (
     <div className="navbar-container">
       <Link to="/">Home</Link>
-      <Link to="/register">Register</Link>
+
+      {/* <Link to="/register">Register</Link>
        <Link to="/login">Login</Link>
        <Link to="/products">Products</Link>
-    <Link to="/dashboard">Dashboard</Link>
+    <Link to="/dashboard">Dashboard</Link> */}
+
+      {!user ? (
+        <>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+        </>
+      ) :
+        (
+          <>
+            <Link to="/products">Products</Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/add-product">Add Products</Link>
+            <Link to="/admin-products">Admin Products</Link>
+
+            <button onClick={logout}>Logout</button>
+          </>
+        )
+      }
     </div>
   )
 }
