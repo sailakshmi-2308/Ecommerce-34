6ab4333dd82056b3a8bf74c3a1be87ac7bc74c58
Sai Lakshmi <sailakshmi@codegnan.com>
2026-06-18 09:55:58 +0530

    login page done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+
+
+  const navigate=useNavigate()
+  const [formData,setFormData]=useState({
+    email:'',
+    password:""
+  })
+
+  function handleChange(e){
+
+    setFormData({
+
+      ...formData,
+      [e.target.name]:e.target.value
+    })
+
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    try{
+      const result=await axios.post("https://ecomflask.duckdns.org/api/admin/login",formData,
+      {
+        withCredentials:true
+      }
+    )
+    console.log(result.data)
+
+    localStorage.setItem("admin",JSON.stringify(result.data.admin))
+
+    navigate("/dashboard")
+
+    }catch (error) {
+      console.log("error response:", error.response);
+      console.log(error.response?.data || error.message);
+    }
+
+
+
+  }
   return (
     <div>
-      Login
+     <h1>Pleast Login here</h1>
+     <form onSubmit={handleSubmit}>
+      <input type="text" 
+      placeholder="enter email"
+      name="email"
+      value={formData.email}
+      onChange={handleChange}
+      />
+
+      <input type="password" 
+      placeholder="enter password"
+      name="password"
+      value={formData.password}
+      onChange={handleChange}
+      />
+  <button>Login</button>
+
+     </form>
     </div>
   )
 }
