a9a1e500d20f3fe2f97be21429b5076a53e6900c
Sai Lakshmi <sailakshmi@codegnan.com>
2026-06-20 10:27:22 +0530

    edit product done

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -1,9 +1,192 @@
-import React from 'react'
+import React ,{useEffect, useState}from 'react'
+import axios from 'axios';
+import { useParams,useNavigate } from 'react-router-dom';
 
 export default function EditProduct() {
+
+  const {id}=useParams()
+  const navigate=useNavigate()
+
+  const [formData, setFormData] = useState({
+      title: "",
+      Description: "",
+      About_item: "",
+      quantity: "",
+      price: "",
+      category: ""
+    });
+
+      const [file, setFile] = useState(null);
+    function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
+
+
+  async function getProduct(){
+   
+    try{
+
+     const result= await axios.get( `https://ecomflask.duckdns.org/api/admin/item/${id}`,{
+        withCredentials:true
+      })
+      console.log("product details:",result.data)
+      console.log(result.data.product)
+      const p=result.data.product
+
+      setFormData({
+          title:p.itemname,
+      Description:p.item_desc,
+      About_item:p.item_about,
+      quantity:p.quantity,
+      price:p.price,
+      category: p.category
+      })
+      setFile(p.image)
+
+
+    }
+   catch (error) {
+      console.log("error response:", error.response);
+      console.log(error.response?.data || error.message);
+    }
+  }
+
+  useEffect(()=>{
+    getProduct()
+  },[])
+
+
+  async function handleSubmit(event){
+    event.preventDefault()
+
+     const data = new FormData();
+
+      data.append("title", formData.title);
+      data.append("Description", formData.Description);
+      data.append("About_item", formData.About_item);
+      data.append("quantity", formData.quantity);
+      data.append("price", formData.price);
+      data.append("category", formData.category);
+     
+      if(file){
+         data.append("file", file);
+      }
+
+      try{
+
+        const result=await axios.put(`https://ecomflask.duckdns.org/api/admin/update-item/${id}`,data,
+          {
+            withCredentials:true
+          }
+
+        )
+        alert("succefully updated");
+        navigate("/admin-products" )
+
+
+      }
+      catch (error) {
+      console.log("error response:", error.response);
+      console.log(error.response?.data || error.message);
+    }
+
+  }
+
   return (
-    <div>
-      
+    <div className="container mt-5">
+      <div className="card shadow p-4">
+        <h2 className="text-center mb-4">Add Product</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Product Title</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter product title"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Product Description</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter product description"
+              name="Description"
+              value={formData.Description}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">About Item</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter about item"
+              name="About_item"
+              value={formData.About_item}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Quantity</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Enter product quantity"
+              name="quantity"
+              value={formData.quantity}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Price</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Enter product price"
+              name="price"
+              value={formData.price}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Category</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter product category"
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="form-label">Upload Product Image</label>
+            <input
+              type="file"
+              className="form-control"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary w-100">
+            Update Product
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
